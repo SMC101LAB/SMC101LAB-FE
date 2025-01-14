c5f0c61a4675ccf20a3e481a7c262a08b39c4f6c
KimDoHyun <kdhqwe1030@naver.com>
2025-01-14 18:04:23 +0900

    feat:회원가입 api연결 및 react-query 환경 구축

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/globalStyle';
 import Router from './Router';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Router />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
