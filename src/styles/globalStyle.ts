dc971c69a55846ded59d14060084e34f3bf8e079
KimDoHyun <kdhqwe1030@naver.com>
2025-01-15 13:51:38 +0900

    init: 글로벌 css변경 및 font 파일 분리

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,7 +1,4 @@
 import { createGlobalStyle } from 'styled-components';
-import PretendardLight from '../assets/fonts/Pretendard-Light.woff2';
-import PretendardMedium from '../assets/fonts/Pretendard-Medium.woff2';
-import PretendardBold from '../assets/fonts/Pretendard-Bold.woff2';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -9,33 +6,8 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-  @font-face {
-    font-family: 'Pretendard';
-    src: url(${PretendardLight}) format('woff2');
-    font-weight: 400;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Pretendard';
-    src: url(${PretendardMedium}) format('woff2');
-    font-weight: 500;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Pretendard';
-    src: url(${PretendardBold}) format('woff2');
-    font-weight: 700;
-    font-style: normal;
-  }
-  
-  html, body, #root, * {
-    font-family: 'Pretendard', sans-serif !important; 
-  }
 
  
-
   button {
     cursor: pointer;
     border: none;
