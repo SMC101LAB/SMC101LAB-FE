b37ad009eea4732857637660206bd3b29be0efc2
KimDoHyun <kdhqwe1030@naver.com>
2025-01-23 02:49:47 +0900

    chore: * 드래그 금지 스타일 추가

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -5,6 +5,10 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-user-select:none;
+    -moz-user-select:none;
+    -ms-user-select:none;
+    user-select:none;
   }
  
   button {
