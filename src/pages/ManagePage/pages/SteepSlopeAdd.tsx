60164637e3f78e60bff1e12d1c2d50adf895ec57
KimDoHyun <kdhqwe1030@naver.com>
2025-01-25 04:37:42 +0900

    chore: 임시 페이지

--- src/pages/ManagePage/pages/SteepSlopeAdd.tsx
+++ src/pages/ManagePage/pages/SteepSlopeAdd.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const SteepSlopeAdd = () => {
   return <div>steepSlopeAdd</div>;
 };
--- src/pages/ManagePage/pages/SteepSlopeModi.tsx
+++ src/pages/ManagePage/pages/SteepSlopeModi.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const SteepSlopeModi = () => {
   return <div>steepSlopeModi</div>;
 };
