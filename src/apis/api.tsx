575c16ffdba05fad7f8cca95a38f7deb39f2bc0f
Kimdohyun <115572203+kdhqwe1030@users.noreply.github.com>
2025-01-26 03:58:26 +0900

    chore: api withCredentials옵션 수정

--- src/apis/api.tsx
+++ src/apis/api.tsx
@@ -5,7 +5,7 @@ export const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
