164a1e54f60a41af6baebc757c4d56ec17058ac1
KimDoHyun <kdhqwe1030@naver.com>
2025-01-27 02:42:30 +0900

    Revert "chore: api 서버 url 환경변수 수정"
    This reverts commit a4ddbda76e1dcb191b7c6f16e9c69eecaf0ba846.

--- src/apis/api.tsx
+++ src/apis/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_ADDRESS}`,
+  baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
   },
