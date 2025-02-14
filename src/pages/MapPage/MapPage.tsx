2c0b2160f3714cf1f63ec7b968b24d6f806ee4bc
KimDoHyun <kdhqwe1030@naver.com>
2025-02-14 19:17:22 +0900

    feat:검색창 ui 구현

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import BottomSheet from './components/BottomSheet';
 import MapComponent from './components/MapComponent';
 import { Slope, slopeAPI } from '../../apis/Map/LookUp';
+import search from '../../assets/Icons/searchWhite.svg';
 
 const MapPage = () => {
   // console.log(escarpmentData);
@@ -50,7 +51,13 @@ const MapPage = () => {
           selectedMarkerId !== null ? slopeData[selectedMarkerId] : null
         }
       />
+      <SearchContainer>
+        <SearchInput placeholder="검색..."></SearchInput>
 
+        <SearchIconWrapper>
+          <SearchIcon src={search} alt="검색" />
+        </SearchIconWrapper>
+      </SearchContainer>
       <AllShowButton
         $isSelect={allTextShow}
         onClick={() => {
@@ -78,18 +85,51 @@ const BaseBackground = styled.div`
 
 const AllShowButton = styled.button<{ $isSelect: boolean }>`
   position: absolute;
-  top: 10px;
+  top: 15px;
   right: 10px;
   border: none;
   border-radius: 8px;
   padding: 5px 10px;
-  box-shadow: 0px 0px 5px #444;
+  box-shadow: ${({ theme }) => theme.shadows.sm};
   font-weight: 550;
   background-color: ${({ $isSelect, theme }) =>
-    $isSelect ? theme.colors.secondary : '#fff'};
+    $isSelect ? theme.colors.primaryDark : '#fff'};
   color: ${({ $isSelect, theme }) =>
-    !$isSelect ? theme.colors.secondary : '#fff'};
+    !$isSelect ? theme.colors.primaryDark : '#fff'};
   &:focus {
     outline: none;
   }
+`;
+
+const SearchContainer = styled.div`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  display: flex;
+  gap: 10px;
+  padding: 5px 10px;
+`;
+
+const SearchInput = styled.input`
+  padding: 5px 15px;
+  border-radius: 20px;
+  /* border: 2px solid;
+  border-color: ${({ theme }) => theme.colors.primaryDark};
+ */
+  background-color: #fff;
+  box-shadow: ${({ theme }) => theme.shadows.lg};
+`;
+const SearchIcon = styled.img`
+  width: 23px;
+  z-index: 1;
+`;
+const SearchIconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: ${({ theme }) => theme.colors.primaryDark};
+  box-shadow: ${({ theme }) => theme.shadows.md};
 `;
