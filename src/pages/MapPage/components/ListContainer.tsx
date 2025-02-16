9c421a0fea7cf672e2e5b6a3b6e3a1b3d5a34d6f
KimDoHyun <kdhqwe1030@naver.com>
2025-02-16 11:35:18 +0900

    fix: 리스트 아이템 디자인 수정

--- src/pages/MapPage/components/ListContainer.tsx
+++ src/pages/MapPage/components/ListContainer.tsx
@@ -15,12 +15,30 @@ const ListContainer: React.FC<ListProps> = ({ item, onClick }) => {
     : 'F';
   return (
     <Container onClick={onClick}>
-      <TopWrapper>
-        <TitleWrapper>
-          <Title>{item.name}</Title>
-          <Num>{item.managementNo}</Num>
-        </TitleWrapper>
-        <GradeValue grade={grade}>{grade}</GradeValue>
+      <Wrapper>
+        <GradeBackground grade={grade}>
+          <GradeText grade={grade}>{grade}</GradeText>
+        </GradeBackground>
+        <MainInnerContainer>
+          <TitleWrapper>
+            <Title>{item.name}</Title>
+            <Num>{item.managementNo}</Num>
+          </TitleWrapper>
+          <div>
+            <AddressText>
+              {item.location.province} {item.location.city}
+              {item.location.district}
+            </AddressText>
+            <SmallAddressText>{item.location.address}</SmallAddressText>
+          </div>
+        </MainInnerContainer>
+      </Wrapper>
+
+      {/* <TopWrapper>
+
+        <GradeBackground grade={grade}>
+          <GradeText grade={grade}>{grade}</GradeText>
+        </GradeBackground>
       </TopWrapper>
       <AddressWrapper>
         <Label>주소</Label>
@@ -31,7 +49,7 @@ const ListContainer: React.FC<ListProps> = ({ item, onClick }) => {
       <AddressWrapper>
         <Label></Label>
         <AddressValue>{item.location.address}</AddressValue>
-      </AddressWrapper>
+      </AddressWrapper> */}
     </Container>
   );
 };
@@ -41,54 +59,46 @@ export default ListContainer;
 const Container = styled.div`
   width: 100%;
   padding: 15px 20px;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.grey[300]};
+  border: 1px solid ${({ theme }) => theme.colors.grey[300]};
+  border-radius: 10px;
 `;
-const TopWrapper = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  gap: 30px;
   align-items: center;
-  justify-content: space-between;
-  padding-bottom: 15px;
-`;
-const TitleWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
 `;
 
-const Title = styled.div`
-  font-size: 22px;
-  color: ${({ theme }) => theme.colors.primary};
+const MainInnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+const GradeBackground = styled.div<{ grade: string }>`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ grade }) => {
+    switch (grade) {
+      case 'A':
+        return 'rgba(46, 204, 113, 0.15)'; // #2ecc71 with opacity
+      case 'B':
+        return 'rgba(241, 196, 15, 0.15)'; // #f1c40f with opacity
+      case 'C':
+        return 'rgba(230, 126, 34, 0.15)'; // #e67e22 with opacity
+      case 'D':
+        return 'rgba(231, 76, 60, 0.15)'; // #e74c3c with opacity
+      default:
+        return 'rgba(51, 51, 51, 0.15)'; // #333333 with opacity
+    }
+  }};
+`;
+const GradeText = styled.div<{ grade: string }>`
+  font-size: 20px;
   font-weight: 600;
-`;
-const InfoRow = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 12px;
-`;
-
-const Label = styled.div`
-  min-width: 30px;
-  font-size: ${({ theme }) => theme.fonts.sizes.ms};
-  color: ${({ theme }) => theme.colors.grey[600]};
-  font-weight: ${({ theme }) => theme.fonts.weights.medium};
-`;
-const Num = styled.div`
-  font-size: 14px;
-  color: #7e7e7e;
-`;
-const AddressWrapper = styled(InfoRow)`
-  align-items: flex-start;
-`;
-const Value = styled.div`
-  font-size: ${({ theme }) => theme.fonts.sizes.ms};
-  color: ${({ theme }) => theme.colors.grey[700]};
-  font-weight: ${({ theme }) => theme.fonts.weights.medium};
-`;
-const AddressValue = styled(Value)`
-  line-height: 1.4;
-`;
-
-const GradeValue = styled(Value)<{ grade: string }>`
   color: ${({ grade }) => {
     switch (grade) {
       case 'A':
@@ -103,5 +113,56 @@ const GradeValue = styled(Value)<{ grade: string }>`
         return '#333';
     }
   }};
-  font-weight: 600;
 `;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+
+const Title = styled.div`
+  font-size: ${({ theme }) => theme.fonts.sizes.ml};
+  color: ${({ theme }) => theme.colors.primary};
+  font-weight: ${({ theme }) => theme.fonts.weights.bold};
+`;
+const Num = styled.div`
+  font-size: 14px;
+  color: #7e7e7e;
+`;
+
+// const TopWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   padding-bottom: 15px;
+// `;
+// const InfoRow = styled.div`
+//   display: flex;
+//   align-items: center;
+//   gap: 12px;
+// `;
+
+// const Label = styled.div`
+//   min-width: 30px;
+//   font-size: ${({ theme }) => theme.fonts.sizes.ms};
+//   color: ${({ theme }) => theme.colors.grey[600]};
+//   font-weight: ${({ theme }) => theme.fonts.weights.medium};
+// `;
+
+// const AddressWrapper = styled(InfoRow)`
+//   align-items: flex-start;
+// `;
+const AddressText = styled.div`
+  font-size: ${({ theme }) => theme.fonts.sizes.ms};
+  color: ${({ theme }) => theme.colors.grey[700]};
+  font-weight: ${({ theme }) => theme.fonts.weights.medium};
+`;
+const SmallAddressText = styled.div`
+  font-size: ${({ theme }) => theme.fonts.sizes.cl};
+  color: ${({ theme }) => theme.colors.grey[600]};
+  font-weight: ${({ theme }) => theme.fonts.weights.medium};
+`;
+// const AddressValue = styled(Value)`
+//   line-height: 1.4;
+// `;
