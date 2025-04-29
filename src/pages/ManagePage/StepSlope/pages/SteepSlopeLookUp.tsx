fc7d821d9afef597298b53c7a5e7c18ee246f1d7
KimDoHyun <kdhqwe1030@naver.com>
2025-04-29 20:27:21 +0900

    refactoring: 열 분리

--- src/pages/ManagePage/StepSlope/pages/SteepSlopeLookUp.tsx
+++ src/pages/ManagePage/StepSlope/pages/SteepSlopeLookUp.tsx
@@ -39,6 +39,10 @@ import TravelExploreRoundedIcon from '@mui/icons-material/TravelExploreRounded';
 import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import { useNotificationStore } from '../../../../hooks/notificationStore';
+import {
+  getSlopeColumns,
+  getDefaultColumnVisibility,
+} from '../components/table/coloums';
 
 const FETCH_SIZE = 50;
 declare module '@tanstack/react-table' {
@@ -51,23 +55,11 @@ declare module '@tanstack/react-table' {
 const SteepSlopeLookUp = () => {
   const queryClient = useQueryClient();
 
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({
-    startLatDegree: false,
-    startLatMinute: false,
-    startLatSecond: false,
-    startLongDegree: false,
-    startLongMinute: false,
-    startLongSecond: false,
-    endLatDegree: false,
-    endLatMinute: false,
-    endLatSecond: false,
-    endLongDegree: false,
-    endLongMinute: false,
-    endLongSecond: false,
-  });
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(
+    getDefaultColumnVisibility()
+  );
   const [columnSizing, setColumnSizing] = useState<ColumnSizingState>({}); // 표 열 변수
   const tableContainerRef = useRef<HTMLDivElement>(null); //표 변수
-  const columnHelper = createColumnHelper<Slope>(); //열 선언 변수
   const [totalCount, setTotalCount] = useState(0); //전체 데이터
   const [searchQuery, setSearchQuery] = useState(''); //검색어쿼리
   const [inputValue, setInputValue] = useState(''); //검색어
@@ -119,292 +111,7 @@ const SteepSlopeLookUp = () => {
   }, [data]);
 
   //데이터 열 선언
-  const columns = useMemo(
-    () => [
-      columnHelper.accessor(
-        (_row, index) => {
-          return index + 1;
-        },
-        {
-          id: 'index',
-          header: '번호',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor('managementNo', {
-        header: '관리번호',
-        size: 120,
-      }),
-      columnHelper.accessor('name', {
-        header: '급경사지명',
-        size: 150,
-      }),
-      columnHelper.accessor((row) => row.management.organization || '', {
-        id: 'organization',
-        header: '시행청명',
-        size: 120,
-      }),
-      columnHelper.accessor((row) => row.management.authority || '', {
-        id: 'authority',
-        header: '관리주체구분코드',
-        size: 150,
-      }),
-      columnHelper.accessor((row) => row.management.department || '', {
-        id: 'department',
-        header: '소관부서명',
-        size: 150,
-      }),
-      columnHelper.accessor((row) => row.location.province, {
-        id: 'province',
-        header: '시도',
-        size: 100,
-      }),
-      columnHelper.accessor((row) => row.location.city, {
-        id: 'city',
-        header: '시군구',
-        size: 120,
-      }),
-      columnHelper.accessor((row) => row.location.district, {
-        id: 'district',
-        header: '읍면동',
-        size: 120,
-      }),
-      columnHelper.accessor((row) => row.location.address || '', {
-        id: 'address',
-        header: '상세주소',
-        size: 200,
-      }),
-      columnHelper.accessor((row) => row.location.roadAddress || '', {
-        id: 'roadAddress',
-        header: '도로명상세주소',
-        size: 120,
-      }),
-      columnHelper.accessor((row) => row.location.mountainAddress || '', {
-        id: 'mountainAddress',
-        header: '산주소여부',
-        size: 90,
-      }),
-      columnHelper.accessor((row) => row.location.mainLotNumber || '', {
-        id: 'mainLotNumber',
-        header: '주지번',
-        size: 60,
-      }),
-      columnHelper.accessor((row) => row.location.subLotNumber || '', {
-        id: 'subLotNumber',
-        header: '부지번',
-        size: 60,
-      }),
-      columnHelper.accessor(
-        (row) => {
-          const start = row.location.coordinates.start;
-          return start
-            ? `${start.coordinates[1].toFixed(
-                6
-              )}°N, ${start.coordinates[0].toFixed(6)}°E`
-            : '';
-        },
-        {
-          id: 'coordinates.start',
-          header: '시점위경도',
-          size: 150,
-        }
-      ),
-
-      columnHelper.accessor(
-        (row) => {
-          const end = row.location.coordinates.end;
-          return end
-            ? `${end.coordinates[1].toFixed(6)}°N, ${end.coordinates[0].toFixed(
-                6
-              )}°E`
-            : '';
-        },
-        {
-          id: 'coordinates.end',
-          header: '종점위경도',
-          size: 150,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.start.startLatDegree || '',
-        {
-          id: 'startLatDegree',
-          header: 'GIS좌표시점위도도',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.start.startLatMinute || '',
-        {
-          id: 'startLatMinute',
-          header: 'GIS좌표시점위도분',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.start.startLatSecond || '',
-        {
-          id: 'startLatSecond',
-          header: 'GIS좌표시점위도초',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.start.startLongDegree || '',
-        {
-          id: 'startLongDegree',
-          header: 'GIS좌표시점경경도도',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.start.startLongMinute || '',
-        {
-          id: 'startLongMinute',
-          header: 'GIS좌표시점경경도분',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.start.startLongSecond || '',
-        {
-          id: 'startLongSecond',
-          header: 'GIS좌표시점경경도초',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.end.endLatDegree || '',
-        {
-          id: 'endLatDegree',
-          header: 'GIS좌표종점점위도도',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.end.endLatMinute || '',
-        {
-          id: 'endLatMinute',
-          header: 'GIS좌표종점점위도분',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.end.endLatSecond || '',
-        {
-          id: 'endLatSecond',
-          header: 'GIS좌표종점위도초',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.end.endLongDegree || '',
-        {
-          id: 'endLongDegree',
-          header: 'GIS좌표종점점위도도',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.end.endLongMinute || '',
-        {
-          id: 'endLongMinute',
-          header: 'GIS좌표종점점위도분',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => row.location.coordinates.end.endLongSecond || '',
-        {
-          id: 'endLongSecond',
-          header: 'GIS좌표종점위도초',
-          size: 60,
-        }
-      ),
-      columnHelper.accessor((row) => row.inspections?.serialNumber ?? '', {
-        id: 'inspections_serialNumber',
-        header: '안전점검일련번호',
-        size: 130,
-      }),
-      columnHelper.accessor((row) => row.inspections?.date ?? '', {
-        id: 'inspectionDate',
-        header: '안전점검일자',
-        size: 120,
-      }),
-      columnHelper.accessor((row) => row.inspections?.result ?? '', {
-        id: 'inspectionResult',
-        header: '안전점검결과코드',
-        size: 130,
-      }),
-      columnHelper.accessor((row) => row.disaster?.serialNumber ?? '', {
-        id: 'serialNumber',
-        header: '재해위험도평가일련번호',
-        size: 180,
-      }),
-      columnHelper.accessor((row) => row.disaster?.riskDate ?? '', {
-        id: 'riskDate',
-        header: '재해위험도평가일자',
-        size: 170,
-      }),
-      columnHelper.accessor((row) => row.disaster?.riskLevel ?? '', {
-        id: 'riskLevel',
-        header: '재해위험도평가등급코드',
-        size: 180,
-      }),
-      columnHelper.accessor((row) => row.disaster?.riskScore ?? '', {
-        id: 'riskScore',
-        header: '재해위험도평가등급코드',
-        size: 180,
-      }),
-      columnHelper.accessor((row) => row.disaster?.riskType ?? '', {
-        id: 'riskType',
-        header: '재해위험도평가종류코드',
-        size: 180,
-      }),
-      columnHelper.accessor((row) => row.collapseRisk?.districtNo || '', {
-        id: 'districtNo',
-        header: '붕괴위험지구번호',
-        size: 130,
-      }),
-      columnHelper.accessor((row) => row.collapseRisk?.districtName || '', {
-        id: 'districtName',
-        header: '붕괴위험지구명',
-        size: 130,
-      }),
-      columnHelper.accessor(
-        (row) => (row.collapseRisk.designated ? '지정' : '미지정'),
-        {
-          id: 'riskDesignation',
-          header: '붕괴위험지구지정여부',
-          size: 160,
-        }
-      ),
-      columnHelper.accessor(
-        (row) => {
-          if (!row.collapseRisk?.designationDate) return '';
-          return row.collapseRisk.designationDate;
-        },
-        {
-          id: 'designationDate',
-          header: '붕괴위험지구지정일자',
-          size: 160,
-        }
-      ),
-
-      columnHelper.accessor((row) => row.maintenanceProject?.type || '', {
-        id: 'maintenanceProject',
-        header: '정비사업유형코드',
-        size: 130,
-      }),
-      columnHelper.accessor((row) => row.maintenanceProject?.year || '', {
-        id: 'maintenanceYear',
-        header: '정비사업년도',
-        size: 120,
-      }),
-    ],
-    []
-  );
-
+  const columns = useMemo(() => getSlopeColumns(), []);
   //필터 선언
   const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
     const itemRank = rankItem(row.getValue(columnId), value);
@@ -485,20 +192,7 @@ const SteepSlopeLookUp = () => {
     setSearchQuery('');
     setSelectedRegion(null);
     refetch();
-    setColumnVisibility({
-      startLatDegree: false,
-      startLatMinute: false,
-      startLatSecond: false,
-      startLongDegree: false,
-      startLongMinute: false,
-      startLongSecond: false,
-      endLatDegree: false,
-      endLatMinute: false,
-      endLatSecond: false,
-      endLongDegree: false,
-      endLongMinute: false,
-      endLongSecond: false,
-    });
+    setColumnVisibility(getDefaultColumnVisibility());
   };
 
   //삭제 수정을 위한 행 선택 state
